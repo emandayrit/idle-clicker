982576be9ef9dfcbc8a707b8921b0751946f754c
Ronin <kkrondy@gmail.com>
2023-06-26 02:16:40 +0800

    AttackAnimation

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,13 +7,21 @@ public class GameManager : MonoBehaviour
 {
     [SerializeField] KeyCode clickerClick;
     [SerializeField] float clickCounter = 0;
+    [SerializeField] GameObject playerArm;
+
+    private Animator attackAnimation;
+
+    private void Start()
+    {
+        attackAnimation = playerArm.GetComponent<Animator>();
+    }
 
     private void Update()
     {
         if (Input.GetKeyDown(clickerClick))
         {
             Debug.Log($"you clicked {clickCounter++}");
+            attackAnimation.SetTrigger("Attack");
         }
     }
-
 }

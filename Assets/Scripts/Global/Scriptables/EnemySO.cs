caafdf8dd8bdf7b6cfe32d538e1a28bb94c7c0ba
emandayrit <adridayrit@gmail.com>
2023-07-15 22:13:29 -0800

    Reworked Scriptable Objects
    -Reworked Player, Enemy, and Items Scriptable Objects
    -Added one template for each new Scriptables

--- Assets/Scripts/Global/Scriptables/EnemySO.cs
+++ Assets/Scripts/Global/Scriptables/EnemySO.cs
@@ -1,12 +1,45 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "EnemySO", menuName = "Enemy")]
+[CreateAssetMenu(fileName = "Enemy Scriptable", menuName = "Enemy")]
 public class EnemySO : ScriptableObject
 {
-    public StatsSO enemyStats;
-    public GameObject enemyPrefab;
-    public int rewardGold;
-    public List<ItemSO> rewardItems;
+    public enum EnemyType { Normal,Unique,Boss};
+
+    [Header("For Status")]
+    public bool isEnemyDead = false;
+    public EnemyType enemyType;
+    public string enemyName;
+    public int enemyLevel;
+    public GameObject enemyBody;
+
+    [Header("For Attributes")] //default attributes
+    public float maxHP = 100;
+    public float health = 100;
+    public float damage = 2;
+    public float defense = 0;
+    [Tooltip("1 is equal to 100%")]
+    public float critChance = 0.05f;
+    [Tooltip("1 is equal to 100%")]
+    public float critDamage = 1.25f;
+    [Tooltip("1 is equal to 100%")]
+    public float evasion = 0.01f;
+
+    [Header("For Skills")]
+    public List<ScriptableObject> activeSkills;
+    public List<ScriptableObject> passiveSkills;
+
+    [Header("For Rewards")]
+    public float goldReward;
+    public float expReward;
+    public List<ScriptableObject> rewards;
+
+    public void ResetHealth()
+    {
+        health = maxHP;
+    }
+
+    
+
+
 }
--- Assets/Scripts/Global/Scriptables/ItemSO.cs
+++ Assets/Scripts/Global/Scriptables/ItemSO.cs
@@ -1,18 +1,55 @@
+using System.Collections.Generic;
+using UnityEditor.ShaderKeywordFilter;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "ItemSO", menuName = "Item")]
+[CreateAssetMenu(fileName = "Item Scriptables", menuName = "Item")]
 public class ItemSO : ScriptableObject
 {
+    [Header("For Item Status")]
     public ItemType itemType;
+    public Sprite itemIcon;
     public string itemName;
-    public int itemPrice;
+    public int itemPrice = 1; //default price
 
-    [Header("For Consumable")]
-    public int healAmount;
+    [Header("For Weapon")]
+    public int weaponLevel;
+    public int damageFlat;
+    [Tooltip("1 is equal to 100%")]
+    public float damagePercent;
+    [Tooltip("1 is equal to 100%")]
+    public float critChancePercent;
+    [Tooltip("1 is equal to 100%")]
+    public float critDamagePercent;
 
-    [Header("For Weapon/Armor")]
-    public int attackIncrease;
-    public int healthIncrease;
+    [Header("For Armor")]
+    public int armorLevel;
+    public int maxHPFlat;
+    [Tooltip("1 is equal to 100%")]
+    public float maxHPPercent;
+    public int defenseFlat;
+    [Tooltip("1 is equal to 100%")]
+    public float defensePercent;
+    [Tooltip("1 is equal to 100%")]
+    public float evasionPercent;
 
-    public enum ItemType{ Consumable, Weapon, Armor, Material }
+    public void RemoveItem(string _itemName)
+    {
+        if (_itemName == itemName)
+        {
+            //do remove the item in the inventory or something.
+        }
+    }
+
+    public void ArmorLevelUp()
+    {
+        armorLevel += 1;
+    }
+
+    public void WeaponLevelUp()
+    {
+        weaponLevel += 1;
+    }
+    
+    //for enums holder
+    public enum ItemType{ Weapon,Armor,Material }
 }
